e1f10b5eb78880f2f3632f1831bceef906f4aa84
yosef-agil <yosefagil8@agil.com>
2025-07-08 21:46:27 +0700

    f

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -16,3 +16,10 @@ export const authenticateToken = (req, res, next) => {
     next();
   });
 };
+
+export const requireAdmin = (req, res, next) => {
+  if (!req.user || req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Admin access required' });
+  }
+  next();
+};
