046ffc7c63d57e809405214945e6470bc9d58791
yosef-agil <yosefagil8@agil.com>
2025-08-12 11:02:31 +0700

    fix: app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-// client/src/App.jsx
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import BookingPage from './pages/BookingPage';
-import AdminDashboard from './pages/AdminDashboard';
-import LoginPage from './pages/LoginPage';
-import PrivateRoute from './components/PrivateRoute';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Toaster } from '@/components/ui/toaster';
-import { initGA, trackPageView } from './utils/analytics';
+import BookingPage from './pages/BookingPage';
+import AdminLoginPage from './pages/AdminLoginPage';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+import { initGA, trackPageView } from './lib/utils/analytics';
+import './App.css';
 
 // Component untuk track page views
 function PageTracker() {
@@ -31,13 +31,14 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<BookingPage />} />
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin/login" element={<AdminLoginPage />} />
+          <Route path="/admin" element={<Navigate to="/admin/dashboard" replace />} />
           <Route
-            path="/admin/*"
+            path="/admin/dashboard"
             element={
-              <PrivateRoute>
+              <ProtectedRoute>
                 <AdminDashboard />
-              </PrivateRoute>
+              </ProtectedRoute>
             }
           />
         </Routes>
